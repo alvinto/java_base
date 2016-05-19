33302b638c6c7ce358385dab965f934664487328
wangshuai <787985556@qq.com>
2016-05-19 16:44:13 +0800

    hashset

--- src/com/yyw/javabase/HashSetTest/HashSetTest.java
+++ src/com/yyw/javabase/HashSetTest/HashSetTest.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * HashSet 存取逻辑
+ * 先判断hashcode是否相等，然后判断equals是否相等，如果相等则不能添加进去
  * Created by wangshuai on 2016/5/19.
  */
 public class HashSetTest {
